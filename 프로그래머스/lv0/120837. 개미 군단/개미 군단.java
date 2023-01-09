10f531aa6c338ec48e8db10534cd3c26e566598d
김도연 <89984853+kdn00@users.noreply.github.com>
2023-01-09 15:05:22 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        while(hp>=5){
+            answer++;
+            hp-=5; 
+        }
+        while(hp>=3){
+            answer++;
+            hp-=3; 
+        } while(hp>=1){
+            answer++;
+            hp-=1; 
+        }
+        return answer;
+    }
+}
